34ea4f86e670aa732d1f031c514b787abf9f7c7a
Joe Ng'ethe <joeynimu@gmail.com>
2020-01-19 00:48:32 +0300

    resolved the paid_by subfield on the Payment query

--- user-service/resolvers.js
+++ user-service/resolvers.js
@@ -1,8 +1,13 @@
 const users = require('./users');
 
+const findUserById = id => users.find(({ user_id }) => user_id === id);
+
 const resolvers = {
   Query: {
     users: () => users
+  },
+  Payment: {
+    paid_by: payment => findUserById(payment.user_id)
   }
 };
 
