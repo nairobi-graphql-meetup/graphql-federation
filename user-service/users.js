b7fc72135506434e3279d2556ec9ece6842f39c0
Joe Ng'ethe <joeynimu@gmail.com>
2020-01-19 00:45:41 +0300

    use user_id is place for id

--- user-service/users.js
+++ user-service/users.js
@@ -1,20 +1,20 @@
 const users = [
   {
-    id: '1',
+    user_id: '1',
     first_name: 'John',
     last_name: 'Doe',
     active: true,
     email: 'doe@doe.com'
   },
   {
-    id: '2',
+    user_id: '2',
     first_name: 'Jane',
     last_name: 'Doe',
     active: true,
     email: 'jane@jane.com'
   },
   {
-    id: '3',
+    user_id: '3',
     first_name: 'Mary',
     last_name: 'poppins',
     active: true,
